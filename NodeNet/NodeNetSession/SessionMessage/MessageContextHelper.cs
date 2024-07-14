eb7f36c85723a6439f0a933a5ddd8eee7acc4355
Vitaliy Novak <vitaliynovak555@gmail.com>
2024-07-14 13:48:01 +0300

    MessageContextHelper | Add method for easely retrieving session message data

--- NodeNet/NodeNetSession/SessionMessage/MessageContextHelper.cs
+++ NodeNet/NodeNetSession/SessionMessage/MessageContextHelper.cs
@@ -8,14 +8,19 @@ using System.Text.Json;
 
 namespace NodeNet.NodeNetSession.SessionMessage
 {
-    public static class MessageContextHelper<T>
+    public static class MessageContextHelper
     {
-        public static T? Parse(MessageContext messageContext)
+        public static T? Parse<T>(MessageContext messageContext)
         {
             var sessionMessage = JsonSerializer.Deserialize<SessionMessage>(messageContext.Message.Data);
             if (sessionMessage is null)
                 return default(T);
             return JsonSerializer.Deserialize<T>(sessionMessage.Data);
+        }
+
+        public static string? GetSessionMessageData(MessageContext messageContext)
+        {
+            return JsonSerializer.Deserialize<SessionMessage>(messageContext.Message.Data)?.Data;
         }
     }
 }
